04c3deecb6440d2fd19d9a77b101e2e887cbb1c4
Ben Levy <bllevy@ubuntu.(none)>
2014-02-12 14:43:20 -0600

    added factories file

---
+++ spec/factories.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :game do
+    title         "Pingas: the revenge of the pinga"
+    search_title  "Pingas the revenge of the pinga"
+    coop          "4+"
+    description   "Arjun searches near and far for the pingas"
+    developer     "Sonic and Friends"
+    esrb_rating   "MA"
+    genres        ["action"]
+    metacritic_rating    "N/A"
+  end
+end

66b57551599d1912f3355b88a2644171a77643ff
Ben Levy <bllevy2@illinois.edu>
2014-02-12 17:32:04 -0600

    made a fully formed test sale and check to see it is valid in gamesalespec file

--- spec/models/game_sale_spec.rb
+++ spec/models/game_sale_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe GameSale do
   let(:game) {FactoryGirl.create(:game)}
-  before {@game_sale = game.game_sales.build(store: "steam", saleamt: 2.95)}
+  before {@game_sale = game.game_sales.build(url: "steampowered.com/pingas",occurrence: DateTime.now(), store: "steam",origamt: 9.95, saleamt: 2.95)}
   subject {@game_sale}
 
   it { should respond_to(:saleamt)}
   it { should respond_to(:store)}
+  it { should be_valid }
 end

049f3fda89d6d4632f600ffb69cd747618452aec
Peter <peter@peter-VirtualBox.(none)>
2014-02-19 13:28:52 -0600

    website should be fully functional

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,15 @@
+
+include GameSearchHelper 
+class GameController < ApplicationController
+  
+  def search
+  	#@results = Game.find_by_title(params[:stuff])
+  	@results = GameSearchHelper.find_game(params[:stuff])
+  end
+  def show
+  	@game = Game.find(params[:id])
+
+  end
+
+
+end

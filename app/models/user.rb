acb83a4db1265189442f87f9f62e05e5958465db
Zhuoyuan Li <zhuoyuan35@gmail.com>
2014-03-01 17:52:01 -0600

    Update user.rb
    li156: editted model for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base  
-	validates :name,		presence: true
-	validates :email,		presence: true
-	validates :username,	presence: true
+	validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+	validates :username,	presence: true, length: { maximum: 50 }, uniqueness: true
 	has_many :games, dependent: :destroy
 end

81633c05d160b35e435384ffef2cbbfe37a0041f
Mikeb924 <mblasingame24@gmail.com>
2014-03-02 17:22:13 -0800

    fixed syntax error introduced in last commit

--- config/initializers/signup_error.rb
+++ config/initializers/signup_error.rb
@@ -3,6 +3,6 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
     html_tag
   else
     errors = Array(instance.error_message).join(',&nbsp;')
-    %(<div class = "error_msg">#{html_tag}<span class="validation-error">&nbsp;#{errors}</span></div>)).html_safe
+    %(<div class = "error_msg">#{html_tag}<span class="validation-error">&nbsp;#{errors}</span></div>).html_safe
   end
 end

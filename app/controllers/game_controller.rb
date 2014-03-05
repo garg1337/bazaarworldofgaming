bc9517515c826663d62f7eb05fc74ce0b2c34794
qyzhu <parmazhu@hotmail.com>
2014-03-05 13:49:10 -0600

    added UI for filter games by metacritic rating

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,10 +6,12 @@ class GameController < ApplicationController
   	#@results = Game.find_by_title(params[:stuff])
 	@results = GameSearchHelper.find_game(params[:stuff])
 	if params[:method] == '1'
-	@results = GameSearchHelper.sort_games_by_metacritic_desc(@results)
+		@results = GameSearchHelper.sort_games_by_metacritic_desc(@results)
 	end
-
-	
+	if params[:method] == '2'
+		@results = GameSearchHelper.sort_games_by_metacritic_asc(@results)
+	end
+	@results = GameSearchHelper.filter_games_by_metacritic(@results,params[:low].to_i,params[:high].to_i)
   end
 
 

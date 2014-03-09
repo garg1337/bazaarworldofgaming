911c6d6dde01d81fff1b976c94b3669a25b53a25
Ben Levy <bllevy2@ubuntu.(none)>
2014-03-09 16:35:52 -0700

    modified game page, included dynamic sales pricing data with link. Shows best price on game page and search. bllevy,blasing2

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,23 +1,31 @@
 
 include GameSearchHelper 
+include GameHelper
+
 class GameController < ApplicationController
   
   def search
   	#@results = Game.find_by_title(params[:stuff])
-	@results = GameSearchHelper.find_game(params[:stuff])
-	if params[:method] == '1'
-		@results = GameSearchHelper.sort_games_by_metacritic_desc(@results)
-	end
-	if params[:method] == '2'
-		@results = GameSearchHelper.sort_games_by_metacritic_asc(@results)
-	end
-	@results = GameSearchHelper.filter_games_by_metacritic(@results,params[:low].to_i,params[:high].to_i)
+     @results = nil
+     if signed_in? && params[:filter] == "true"
+       @results = GameSearchHelper.find_and_filter_games(params[:stuff],current_user)
+     else
+       @results = GameSearchHelper.find_game(params[:stuff])
+     end
+	   if params[:method] == '1'
+		   @results = GameSearchHelper.sort_games_by_metacritic_desc(@results)
+	   end
+	   if params[:method] == '2'
+		    @results = GameSearchHelper.sort_games_by_metacritic_asc(@results)
+	   end
+	   @results = GameSearchHelper.filter_games_by_metacritic(@results,params[:low].to_i,params[:high].to_i)
   end
 
 
   def show
   	@game = Game.find(params[:id])
   	@currentID = params[:id]
+    @best = GameHelper.best_price(@game)
 
   end
 
--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,2 +1,14 @@
 module GameHelper
+
+	def best_price(game)
+		best = nil
+		if(game != nil)
+    		game.game_sales.each do |sale|
+        		if best == nil || (sale.saleamt < best.saleamt)
+           			best=sale
+        		end
+        	end
+        end
+        return best
+	end
 end

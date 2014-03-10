0130c872756a37224180e9a3854170c47ca0d66b
Ben Levy <bllevy2@ubuntu.(none)>
2014-03-09 19:09:33 -0500

    changed best price to use db call(order/first), modified sales well. bllevy2,blasing2

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,14 +1,6 @@
 module GameHelper
 
 	def best_price(game)
-		best = nil
-		if(game != nil)
-    		game.game_sales.each do |sale|
-        		if best == nil || (sale.saleamt < best.saleamt)
-           			best=sale
-        		end
-        	end
-        end
-        return best
+		return game.game_sales.order(saleamt: :asc).first
 	end
 end

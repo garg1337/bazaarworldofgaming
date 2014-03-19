059e6f4adeb41ce4f4b83f79a2c8ee0cbbc7d227
Jiwoong <yjw9012@hotmail.com>
2014-03-19 12:22:05 -0500

    Fixed game_search_helper_spec.rb to reflect refactoring

--- spec/helpers/game_search_helper_spec.rb
+++ spec/helpers/game_search_helper_spec.rb
@@ -39,7 +39,7 @@ describe GameSearchHelper do
 
  		it "should have more relevant games on the top of the list" do
 	 		words_list = ["command", "conquer", "red", "alert"]
-	 		games_list = GameSearchHelper.getGameLisPartialMatch(words_list)
+	 		games_list = GameSearchHelper.get_game_lis_partial_match(words_list)
 	 		
 			puts
 	 		games_list.each do |game|
@@ -57,13 +57,13 @@ describe GameSearchHelper do
 
  		it "should return the substring before the first colon" do
 	 		title = "civilization V: breve new world"
-	 		result = GameSearchHelper.handleColon(title)
+	 		result = GameSearchHelper.handle_colon(title)
 	 		expect(result).to eq("civilization V")
  		end
 
  		it "should return nil" do
 	 		title = "halo 2"
-	 		result = GameSearchHelper.handleColon(title)
+	 		result = GameSearchHelper.handle_colon(title)
 	 		expect(result).to eq(nil)
  		end
 
@@ -73,19 +73,19 @@ describe GameSearchHelper do
 
  		it "should be empty" do
 	 		words_list = ["a", "b", "c", "d", "e"]
-	 		GameSearchHelper.delChar(words_list)
+	 		GameSearchHelper.del_char(words_list)
 	 		expect(words_list.size).to eq(0)
  		end
 
  		it "should have only one element ef left" do
  			words_list = ["a", "b", "c", "d", "ef"]
-	 		GameSearchHelper.delChar(words_list)
+	 		GameSearchHelper.del_char(words_list)
 	 		expect(words_list.size).to eq(1)
  		end
 
  		it "should have two elements left" do
  			words_list = ["aa", "b", "c", "d", "ef"]
-	 		GameSearchHelper.delChar(words_list)
+	 		GameSearchHelper.del_char(words_list)
 	 		expect(words_list.size).to eq(2)
 	 	end
 
@@ -227,7 +227,7 @@ describe GameSearchHelper do
  		end
 
  		it "should check if search_title in DB includes 'edition'" do
- 			@assassininfo = {title: "Assassin's Creed Edition", 
+ 			@assassininfo = {title: "CS428 Edition", 
 				release_date: nil, 
 				description: "The game centers on the use of a machine named the 'Animus', which allows the viewing of the protagonist's genetic memories of his ancestors.Through this plot device, details emerge of a struggle between two factions, the Knights Templar and the Assassins (Hashshashin), over an artifact known as a 'Piece of Eden' and the game primarily takes place during the Third Crusade in the Holy Land in 1191.", 
 				esrb_rating: "M - Mature", 
@@ -238,11 +238,11 @@ describe GameSearchHelper do
 				developer: "Ubisoft Montreal", 
 				image_url: "http://thegamesdb.net/banners/boxart/original/front/12-1.jpg", 
 				genres: ["Shooter"],
-				search_title:"assassins creed edition"}
+				search_title:"cs428 edition"}
 
 			Game.create(@assassininfo)
 
-			result = GameSearchHelper.find_right_game("Assassin's Creed", "")
+			result = GameSearchHelper.find_right_game("CS428", "")
  			result.should_not be_nil
  			expect(result[:title]).to eq(@assassininfo[:title])
  		end
